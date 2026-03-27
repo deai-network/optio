36093f0f006c809c88c1e7a7d719ad5958784a1a
Kristof Csillag <kristof.csillag@deai-labs.com>
2026-03-27 21:42:46 +0100

    chore: remove build artifacts from optio-api

--- packages/optio-api/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { listProcesses, getProcess, getProcessTree, getProcessLog, getProcessTreeLog, launchProcess, cancelProcess, dismissProcess, resyncProcesses, type ListQuery, type PaginationQuery, type TreeLogQuery, type CommandResult, } from './handlers.js';
-export { publishLaunch, publishResync } from './publisher.js';
-export { createListPoller, createTreePoller, type StreamPollerOptions, type TreePollerOptions, type ListPollerHandle } from './stream-poller.js';
-//# sourceMappingURL=index.d.ts.map
