d8ede46aa069ae8f2a8ec328690f4f536e68dd62
Kristof Csillag <kristof.csillag@deai-labs.com>
2026-03-27 21:42:36 +0100

    chore: remove build artifacts from optio-contracts

--- packages/optio-contracts/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { ObjectIdSchema, PaginationQuerySchema, PaginatedResponseSchema, ErrorSchema, DateSchema } from './schemas/common.js';
-export { ProcessSchema, ProcessStateSchema, LogEntrySchema } from './schemas/process.js';
-export type { Process, ProcessState, LogEntry } from './schemas/process.js';
-export { processesContract } from './contract.js';
-//# sourceMappingURL=index.d.ts.map
--- packages/optio-contracts/dist/index.js
+++
@@ -1,6 +0,0 @@
-// Schemas
-export { ObjectIdSchema, PaginationQuerySchema, PaginatedResponseSchema, ErrorSchema, DateSchema } from './schemas/common.js';
-export { ProcessSchema, ProcessStateSchema, LogEntrySchema } from './schemas/process.js';
-// Contract
-export { processesContract } from './contract.js';
-//# sourceMappingURL=index.js.map
